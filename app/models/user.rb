e4c875e91236a267e4917da1e0855c00e2807e13
koki kanasaka <k.koki980701@gmail.com>
2021-07-13 16:17:26 +0900

    feat：postsのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts, dependent: :destroy
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.name = "ゲスト"
