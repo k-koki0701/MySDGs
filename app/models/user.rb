6d7a1bd598dde2263f59f99b928a0d3c6a6b6a94
koki kanasaka <k.koki980701@gmail.com>
2021-07-13 11:37:23 +0900

    style：end忘れを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
-
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
-    user.name = "ゲスト"
-    user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+      user.password = SecureRandom.urlsafe_base64
+    end
   end
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
 end
