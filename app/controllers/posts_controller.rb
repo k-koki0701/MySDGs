3474c72e7064a4b698d0129cbda9e106b1cb3319
koki kanasaka <k.koki980701@gmail.com>
2021-07-20 11:15:07 +0900

    fix：誤字修正（ceate→create）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def ceate
+  def create
     @post = Post.new(post_params)
     if @post.save
       redirect_to posts_path, notice: "投稿しました！"
