3818b66eddac104e5b5aed848c02671a26f9790d
koki kanasaka <k.koki980701@gmail.com>
2021-07-20 11:31:59 +0900

    feat：投稿を現在のログインユーザーと紐付ける

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
     if @post.save
       redirect_to posts_path, notice: "投稿しました！"
     else
@@ -20,7 +21,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content, :user_id)
+    params.require(:post).permit(:content)
   end
 
 end
