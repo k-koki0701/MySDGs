7f049cbd6f2b915eb3f3f1b3493088bd5a94aa3f
koki kanasaka <k.koki980701@gmail.com>
2021-07-20 20:54:52 +0900

    feat：pictureとのアソシエーション関係記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  has_many :pictures, dependent: :destroy
+  accepts_nested_attributes_for :pictures
+
   validates :content, presence: true
 end
