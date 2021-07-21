8feceb6302ca412cd025a107bc8c6b46ab475573
koki kanasaka <k.koki980701@gmail.com>
2021-07-21 23:25:24 +0900

    feat：デフォルト画像登録

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,6 +21,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def default_url(*args)
+    'default.png'
+  end
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
