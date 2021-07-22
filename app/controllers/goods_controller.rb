41a82cdf231ee52fea298c388eb339c6a4f1aa02
koki kanasaka <k.koki980701@gmail.com>
2021-07-22 23:44:19 +0900

    feat：destroyアクション作成

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,4 +3,10 @@ class GoodsController < ApplicationController
     favorite = current_user.goods.create(post_id: params[:post_id])
     redirect_to posts_path
   end
+
+  def destroy
+    favorite = current_user.goods.find_by(id: params[:id]).destroy
+    redirect_to posts_path
+  end
+
 end
