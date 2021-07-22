c0a279e32da9a6bb5430e0aea991899b961dcd90
koki kanasaka <k.koki980701@gmail.com>
2021-07-23 00:07:25 +0900

    feat：いいねを投稿詳細画面に表示させる

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,11 +1,11 @@
 class GoodsController < ApplicationController
   def create
-    favorite = current_user.goods.create(post_id: params[:post_id])
+    good = current_user.goods.create(post_id: params[:post_id])
     redirect_to posts_path
   end
 
   def destroy
-    favorite = current_user.goods.find_by(id: params[:id]).destroy
+    good = current_user.goods.find_by(id: params[:id]).destroy
     redirect_to posts_path
   end
 
