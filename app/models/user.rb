5ac3b00f3e62ea29cf18ea870cfd984270a2800f
koki kanasaka <k.koki980701@gmail.com>
2021-07-23 10:22:09 +0900

    feat：relationshipのアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :goods, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
