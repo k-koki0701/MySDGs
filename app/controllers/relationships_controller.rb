90efa4050174892448b135b1236feca24de59a61
koki kanasaka <k.koki980701@gmail.com>
2021-07-23 10:53:04 +0900

    feat：存在するアクションのrespondを全てjsで返す設定

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,12 @@
 class RelationshipsController < ApplicationController
+
+  respond_to? :js
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
   end
 
   def destroy
+
   end
 end
