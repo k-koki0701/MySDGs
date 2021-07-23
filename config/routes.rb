1cb8c2a4157c4eadb2c6fe0616595dc4f6136eb0
koki kanasaka <k.koki980701@gmail.com>
2021-07-23 10:44:58 +0900

    feat：relationshipsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :users, only: [:show]
   resources :goods, only: [:index, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
