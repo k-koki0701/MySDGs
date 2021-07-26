3d7195509d8f65be700ab795a21cdcf8bd8229bb
koki kanasaka <k.koki980701@gmail.com>
2021-07-26 11:53:42 +0900

    feat：ユーザー一覧でフォローできる機能追加 #19

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,8 +2,8 @@ class RelationshipsController < ApplicationController
 
   respond_to? :js
   def create
-    @user = User.find(params[:relationship][:followed_id])
-    current_user.follow!(@user)
+      @user = User.find(params[:relationship][:followed_id])
+      current_user.follow!(@user)
   end
 
   def destroy
