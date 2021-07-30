59fd1c53c452b17cc9147c82cc5bc164763b95a6
koki kanasaka <k.koki980701@gmail.com>
2021-07-30 12:41:20 +0900

    feat：commentsテーブル作成 #33

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_011123) do
+ActiveRecord::Schema.define(version: 2021_07_30_033359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "goods", force: :cascade do |t|
     t.bigint "user_id"
@@ -70,6 +80,8 @@ ActiveRecord::Schema.define(version: 2021_07_23_011123) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "goods", "posts"
   add_foreign_key "goods", "users"
   add_foreign_key "pictures", "posts"
