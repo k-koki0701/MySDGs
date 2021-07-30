bd4a5cb5babb8c32f981d220dad2cf07e2b357af
koki kanasaka <k.koki980701@gmail.com>
2021-07-30 13:00:41 +0900

    feat：commentのアソシエーション設定 #33

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_many :goods, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :good_users, through: :goods, source: :user
   has_many :pictures, dependent: :destroy
   accepts_nested_attributes_for :pictures
