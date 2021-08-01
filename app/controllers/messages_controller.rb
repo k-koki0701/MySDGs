3301c371fb9aa86400ae39a0f119e01f608f5f79
koki kanasaka <k.koki980701@gmail.com>
2021-08-01 16:55:40 +0900

    feat：createアクションを作成 #37

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,4 +19,13 @@ class MessagesController < ApplicationController
     @messages = @messages.order(:created_at)
     @message = @conversation.messages.build
   end
+
+  def create
+    @message = @conversation.messages.build(message_params)
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    else
+      render 'index'
+    end
+  end
 end
