2125612d73422871967f59251002701e77dff5e0
koki kanasaka <k.koki980701@gmail.com>
2021-08-01 23:50:28 +0900

    feat：eventsコントローラーにcreateアクションを作成 #38

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,21 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+    @event.owner = current_user
+    if @event.save
+      redirect_to events_path, notice: "作成しました！"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :content, :schedule, :owner_id)
+  end
+
+
 end
