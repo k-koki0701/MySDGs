927809d26cf7ff1072c91928c97893f84b8a2a97
koki kanasaka <k.koki980701@gmail.com>
2021-08-01 15:38:02 +0900

    feat：ストロングパラメータを定義 #37

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -13,4 +13,9 @@ class ConversationsController < ApplicationController
     redirect_to conversation_messages_path(@conversation)
   end
 
+  private
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
+
 end
