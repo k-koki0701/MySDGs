2056cef8b3b803f4d325cc34e05c8231b1de2cfb
koki kanasaka <k.koki980701@gmail.com>
2021-08-02 01:35:30 +0900

    feat：owner_idのアソシエーションを設定 #38

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :goods, dependent: :destroy
   has_many :participations, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :events, foreign_key: :owner_id
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
