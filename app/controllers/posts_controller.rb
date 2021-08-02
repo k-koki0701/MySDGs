624ccabebf4cc896c4e33f3654edd2982191c7a9
koki kanasaka <k.koki980701@gmail.com>
2021-08-02 18:39:51 +0900

    feat：投稿時にカテゴリー選択を可能にした #39

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content, pictures_attributes: [:id, :image,:image_cache,:remove_image])
+    params.require(:post).permit(:content, pictures_attributes: [:id, :image,:image_cache,:remove_image], category_ids: [])
   end
 
   def set_post
