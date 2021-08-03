32257b739e9ec866d0739d93495e048c4a0bc89c
koki kanasaka <k.koki980701@gmail.com>
2021-08-04 08:27:24 +0900

    feat：userテーブルにadminカラムを追加 #46

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_103953) do
+ActiveRecord::Schema.define(version: 2021_08_03_232557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_103953) do
     t.text "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
