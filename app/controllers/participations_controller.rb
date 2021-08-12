81a9348d118b9b9d6cb871268a0aeb94aeb51363
koki kanasaka <k.koki980701@gmail.com>
2021-08-12 20:34:05 +0900

    fix：,忘れを修正　#68

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,5 +1,5 @@
 class ParticipationsController < ApplicationController
-  before_action :authenticate_user!, only: [:create :destroy]
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def index
     @participations = current_user.participations.all
