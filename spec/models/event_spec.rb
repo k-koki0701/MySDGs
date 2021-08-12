be32be1c74fe01c249fe7ce9de7d7cee013f1de8
koki kanasaka <k.koki980701@gmail.com>
2021-08-12 15:45:00 +0900

    style：誤字修正 #67

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe '投稿モデル機能', type: :model do
+describe 'イベントモデル機能', type: :model do
   describe 'バリデーションのテスト' do
-    context '投稿の本文が空の場合' do
+    context 'イベントのタイトルが空の場合' do
       it 'バリデーションにひっかかる' do
         user = FactoryBot.create(:user)
         category = FactoryBot.create(:category)
@@ -10,32 +10,38 @@ describe '投稿モデル機能', type: :model do
         expect(post).not_to be_valid
       end
     end
-    context '投稿のカテゴリーが空の場合' do
+    context 'イベントの本文が空の場合' do
       it 'バリデーションにひっかかる' do
         user = FactoryBot.create(:user)
         post = Post.new(content: 'ゴミ拾い', category_ids: "", user_id: user.id )
         expect(post).not_to be_valid
       end
     end
-    context '投稿の本文とカテゴリーに内容が記載されている場合' do
-      it 'バリデーションが通る' do
+    context 'イベントのカテゴリーが空の場合' do
+      it 'バリデーションにひっかかる' do
         user = FactoryBot.create(:user)
         category = FactoryBot.create(:category)
         post = Post.new(content: 'ゴミ拾い', category_ids: category.id, user_id: user.id)
         expect(post).to be_valid
       end
     end
-    context '投稿の本文とカテゴリーに内容が記載され、画像が登録された場合' do
-      it 'バリデーションが通る' do
+    context 'イベントの画像が空の場合' do
+      it 'バリデーションにひっかかる' do
         post = FactoryBot.create(:post)
         expect(post).to be_valid
       end
     end
-    context '投稿の本文とカテゴリーに内容が記載され、画像が複数枚登録された場合' do
+    context 'イベントの日時が空の場合' do
+      it 'バリデーションにひっかかる' do
+        post = FactoryBot.create(:post)
+        expect(post).to be_valid
+      end
+    end
+    context '' do
       it 'バリデーションが通る' do
         user = FactoryBot.create(:user)
         post = FactoryBot.create(:post_pictures, user_id: user.id)
-        expect(post).to be_valid
+        expect(guide).to be_valid
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 describe 'ユーザーモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'ユーザーの名前が空の場合' do
-      it 'バリデーションにひっかる' do
-        user = User.new(name: '', email: 'test1@example.com', password: 'password1')
-        expect(user).not_to be_valid
+      it 'バリデーションにひっかかる' do
+        event = User.new(title: '', content: '海でゴミ拾いします！', : 'password1')
+        expect(event).not_to be_valid
       end
     end
     context 'メールアドレスが空の場合' do
-      it 'バリデーションにひっかる' do
+      it 'バリデーションにひっかかる' do
         user = User.new(name: 'テスト太郎1', email: '', password: 'password1')
         expect(user).not_to be_valid
       end
     end
     context 'パスワードが空の場合' do
-      it 'バリデーションにひっかる' do
+      it 'バリデーションにひっかかる' do
         user = User.new(name: 'テスト太郎1', email: 'test1@example.com', password: '')
         expect(user).not_to be_valid
       end
