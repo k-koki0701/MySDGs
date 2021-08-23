3755efc8ff3446de3c09fddc9f51ede9c360a1ae
koki kanasaka <k.koki980701@gmail.com>
2021-08-23 11:43:50 +0900

    feat：投稿一覧画面でいいねを非同期にする #126

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -6,13 +6,15 @@ class GoodsController < ApplicationController
   end
 
   def create
-    good = current_user.goods.create(post_id: params[:post_id])
-    redirect_to posts_path
+    @post = Post.find(params[:post_id])
+    good = current_user.goods.build(post_id: params[:post_id])
+    good.save
   end
 
   def destroy
-    good = current_user.goods.find_by(id: params[:id]).destroy
-    redirect_to posts_path
+    @post = Post.find(params[:post_id])
+    good = Good.find_by(post_id: params[:post_id], user_id: current_user.id)
+    good.destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments, only: [:create]
+    resources :goods, only: [:index, :create, :destroy]
   end
   resources :conversations do
     resources :messages
@@ -18,7 +19,6 @@ Rails.application.routes.draw do
     resources :participations, only: [:index, :create, :destroy]
   end
   resources :users, only: [:index, :show]
-  resources :goods, only: [:index, :create, :destroy]
   resources :relationships, only: [:create, :destroy] do
     member do
       get :followed, :follower
