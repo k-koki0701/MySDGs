791f615ea48a5159bef381f965f6edb09a14a6af
koki kanasaka <k.koki980701@gmail.com>
2021-09-23 13:34:57 +0900

    clean：userコントローラーのindeアクションを削除 #190

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
@@ -11,13 +12,17 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :goods, only: %i[index create destroy]
   end
+
   resources :conversations do
     resources :messages
   end
+
   resources :events do
     resources :participations, only: %i[index create destroy]
   end
-  resources :users, only: %i[index show]
+
+  resources :users, only: %i[show]
+
   resources :relationships, only: %i[create destroy] do
     member do
       get :followed, :follower
@@ -28,4 +33,5 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
   end
+
 end
