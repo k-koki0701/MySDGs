c774f0970924e04d6841f52a03fbec09d0c86a57
koki kanasaka <k.koki980701@gmail.com>
2021-10-02 23:59:53 +0900

    feat：scope追加 #196

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,9 @@ class Post < ApplicationRecord
   validates :category_ids, presence: true
 
   scope :categories_ranking_month, -> { joins(:categories).group("categories.name").where(created_at: Time.current.all_month).order('count_all DESC').limit(3).count }
+
   scope :categories_ranking_year, -> { joins(:categories).group("categories.name").where(created_at: Time.current.all_year).order('count_all DESC').limit(3).count }
+
+  scope :post_list, -> { all.includes(:user, :pictures, :goods).order('id DESC') }
 
 end
